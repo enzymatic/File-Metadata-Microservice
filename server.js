b4a8d52588e72753e867aae4c1add37dafc6e5be
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-10 14:35:07 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
+
+const upload = multer({ dest: 'public/files' });
 
 var app = express();
 
@@ -8,13 +11,15 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // console.log(req.file);
+  // res.send(req.file);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
